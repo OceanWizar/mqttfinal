87556a760831bdadf92e5562c8d9fc11c09cd757
OceanWizar <waltermarcelo01@gmail.com>
2022-12-02 00:18:41 -0300

    Initial commit

---
+++ app/src/main/java/cl/trabajo/mqttwal/MainActivity.java
@@ -0,0 +1,75 @@
+package cl.trabajo.mqttwal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MqttAndroidClient client;
+    private final MemoryPersistence persistence = new MemoryPersistence();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //++++++++++++++++++++++++++++++++++++++++++++
+        final MqttAndroidClient mqttAndroidClient = new MqttAndroidClient(this.getApplicationContext(), "tcp://209.126.106.184:1883", "SensorSantos", persistence);
+        mqttAndroidClient.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                System.out.println("Connection was lost!");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                System.out.println("Message Arrived!: " + topic + ": " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                System.out.println("Delivery Complete!");
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setCleanSession(true);
+        try {
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    System.out.println("Connection Success!");
+                    try {
+                        System.out.println("Subscribing to walter/test");
+                        mqttAndroidClient.subscribe("walter/test", 0);
+                        System.out.println("Subscribed to walter/test");
+                        System.out.println("Publishing message..");
+                        mqttAndroidClient.publish("walter/test", new MqttMessage("Has ingresado correctamente".getBytes()));
+                    } catch (MqttException ex) {
+
+                    }
+                }
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    System.out.println("Connection Failure!");
+                    System.out.println("throwable: " + exception.toString());
+                }
+            });
+        } catch (MqttException ex) {
+            System.out.println(ex.toString());
+        }
+
+
+        //++++++++++++++++++++++++++++++++++++++++++++++
+    }
+}
